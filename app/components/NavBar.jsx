8ed14d70a5ab6256470af10f449307fd9d3d3680
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2024-02-21 23:25:01 -0300

    hero fixed

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,43 +1,43 @@
-'use client'
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import CssBaseline from '@mui/material/CssBaseline';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AboutIcon from '../assets/svg/AboutIcon';
-import ContactIcon from '../assets/svg/ContactIcon';
-import EducationIcon from '../assets/svg/EducationIcon';
-import HomeIcon from '../assets/svg/HomeIcon';
-import ProjectsIcon from '../assets/svg/ProjectsIcon';
-import SkillsIcon from '../assets/svg/SkillsIcon';
-import Logo from '../assets/svg/Logo';
-import { Link } from 'react-scroll';
-import About from './About';
+"use client";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AboutIcon from "../assets/svg/AboutIcon";
+import ContactIcon from "../assets/svg/ContactIcon";
+import EducationIcon from "../assets/svg/EducationIcon";
+import HomeIcon from "../assets/svg/HomeIcon";
+import ProjectsIcon from "../assets/svg/ProjectsIcon";
+import SkillsIcon from "../assets/svg/SkillsIcon";
+import Logo from "../assets/svg/Logo";
+import { Link } from "react-scroll";
+import About from "./About";
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(0),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginRight: -drawerWidth,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
@@ -49,20 +49,20 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
      * those that appear earlier. Since the Drawer comes after the Main content, this adjustment ensures
      * proper interaction with the underlying content.
      */
-    position: 'relative',
-  }),
+    position: "relative",
+  })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -70,13 +70,13 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-start',
+  justifyContent: "flex-start",
 }));
 
 export default function PersistentDrawerRight() {
@@ -92,94 +92,111 @@ export default function PersistentDrawerRight() {
   };
 
   return (
-    <Box sx={{ display: 'flex'}}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open} sx={{
-           background: 'linear-gradient(270deg, #342054, #F220C4)',
-           backgroundSize: '200% 200%',
-           
-           animation: 'gradient 10s ease infinite',
-           "@keyframes gradient": {
-             '0%': {
-               backgroundPosition: '0% 50%',
-             },
-             '50%': {
-               backgroundPosition: '100% 50%',
-             },
-             '100%': {
-               backgroundPosition: '0% 50%',
-             },
-           },
-        
-        }}>
+      <AppBar
+        position="fixed"
+        open={open}
+        sx={{
+          background: "linear-gradient(270deg, #342054, #F220C4)",
+          backgroundSize: "200% 200%",
+
+          animation: "gradient 10s ease infinite",
+          "@keyframes gradient": {
+            "0%": {
+              backgroundPosition: "0% 50%",
+            },
+            "50%": {
+              backgroundPosition: "100% 50%",
+            },
+            "100%": {
+              backgroundPosition: "0% 50%",
+            },
+          },
+        }}
+      >
         <Toolbar>
           <Box noWrap sx={{ flexGrow: 1 }} component="div">
-            <Logo IconColor={'white'}/>
+            <Logo IconColor={"white"} />
           </Box>
           <IconButton
             color="primary"
             aria-label="open drawer"
             edge="end"
             onClick={handleDrawerOpen}
-            sx={{ ...(open && { display: 'none' }) }}
+            sx={{ ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Main open={open} >
+      <Main open={open} id="Inicio">
         <DrawerHeader />
-       
       </Main>
       <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            backgroundColor: '#181D2A'
+            backgroundColor: "#181D2A",
           },
-          
-          
         }}
-        
         variant="persistent"
         anchor="right"
         open={open}
       >
-        <DrawerHeader >
-          <IconButton onClick={handleDrawerClose} sx={{color:"white"}}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose} sx={{ color: "white" }}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </DrawerHeader>
-      
-        <List sx={{color:"#ffffff"}}>
-        
-          {["Inicio","Sobre mi", 'Habilidades', 'Proyectos', "Educación" , "Contacto"].map((text, index) => (
-            console.log(text),
-            <Link to={text} smooth={true} duration={300}>
-            <ListItem key={text} disablePadding >
-              <ListItemButton >
-                <ListItemIcon>
-                {(() => {
-                    switch (index) {
-                    case 0: return <HomeIcon IconColor={'#ffffff'}/>;
-                    case 1: return <AboutIcon IconColor={'#ffffff'}/>;
-                    case 2: return <SkillsIcon IconColor={'#ffffff'}/>;
-                    case 3: return <ProjectsIcon IconColor={'#ffffff'}/>;
-                    case 4: return <EducationIcon IconColor={'#ffffff'}/>;
-                    case 5: return <ContactIcon IconColor={'#ffffff'}/>;
-                }})()}
-                </ListItemIcon>
-                <ListItemText primary={text}/>
-              </ListItemButton>
-            </ListItem>
-            </Link>
-          ))}
-          
+
+        <List sx={{ color: "#ffffff" }}>
+          {[
+            "Inicio",
+            "Sobre mi",
+            "Habilidades",
+            "Proyectos",
+            "Educación",
+            "Contacto",
+          ].map(
+            (text, index) => (
+              console.log(text),
+              (
+                <Link to={text} smooth={true} duration={300}>
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {(() => {
+                          switch (index) {
+                            case 0:
+                              return <HomeIcon IconColor={"#ffffff"} />;
+                            case 1:
+                              return <AboutIcon IconColor={"#ffffff"} />;
+                            case 2:
+                              return <SkillsIcon IconColor={"#ffffff"} />;
+                            case 3:
+                              return <ProjectsIcon IconColor={"#ffffff"} />;
+                            case 4:
+                              return <EducationIcon IconColor={"#ffffff"} />;
+                            case 5:
+                              return <ContactIcon IconColor={"#ffffff"} />;
+                          }
+                        })()}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  </ListItem>
+                </Link>
+              )
+            )
+          )}
         </List>
-        
-        
       </Drawer>
     </Box>
   );
