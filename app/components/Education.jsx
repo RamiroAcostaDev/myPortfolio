79accaec95fca26de33473c8afcacf8eaf60ebd9
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2024-03-09 22:28:55 -0300

    elements align

--- app/components/Education.jsx
+++ app/components/Education.jsx
@@ -1,15 +1,38 @@
 import { Container, Box, Typography } from "@mui/material";
 
-import { Allerta_Stencil, Bruno_Ace, Roboto } from "next/font/google";
+import { Noto_Sans_Thai } from "next/font/google";
 
-const BrunoAce = Bruno_Ace({ subsets: ["latin"], weight: ["400"] });
+const NotoFont = Noto_Sans_Thai({
+  subsets: ["thai"],
+  weight: ["100", "200", "300", "400", "500", "700"],
+});
 
-const EducationItem = [
+const educationItem = [
   {
     title: "Full Stack JavaScrip - ReactJs",
     description: "Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires",
     date: "2023",
-    direction: { borderWidth: "3px 3px 0 0" },
+  },
+  {
+    title: "Full Stack Python - Django",
+    description: "Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires",
+    date: "2022",
+  },
+  {
+    title: "Full Stack Python",
+    description: "Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires",
+    date: "2023",
+  },
+  {
+    title: "Abogado ",
+    description: "Universidad de Buenos Aires.",
+    date: "2019",
+  },
+  {
+    title: " Técnico en computación ",
+    description:
+      "Escuela de Educación Técnica Nro. 2 - Pbro. José María Colombo - Gualeguaychú, Entre Ríos.",
+    date: "2008",
   },
 ];
 
@@ -19,45 +42,65 @@ export default function Education() {
       <Box
         height={"100vh"}
         display={"flex"}
-        justifyContent={"center"}
         alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+        gap={2}
       >
-        <Box
-          display={"flex"}
-          alignContent={"center"}
-          justifyContent={"center"}
-          gap={4}
-          color={"white"}
+        <Typography
+          variant="h4"
+          fontSize={{ xs: 30, lg: 40 }}
+          fontWeight="500"
+          color={"#ffffff"}
+          className={`${NotoFont.className} antialiased`}
         >
-          <Box
-            width={"100px"}
-            display={"flex"}
-            alignContent={"center"}
-            justifyContent={"center"}
-            flexDirection={"column"}
-          >
-            <Typography variant="h6" fontSize={7} textAlign={"center"}>
-              Full Stack JavaScrip - ReactJs
-            </Typography>
-            <Typography variant="p" fontSize={5} textAlign={"center"}>
-              Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires
-            </Typography>
-            <Typography variant="p" fontSize={5} textAlign={"center"}>
-              2023
-            </Typography>
-          </Box>
-
-          <Box width={"100px"}>
+          Educacion
+        </Typography>
+        {educationItem.map(({ title, description, date }, index) => {
+          const isEven = index % 2 === 0;
+          const isLast = index === educationItem.length - 1;
+          return (
             <Box
-              width={"50px"}
-              height={"100%"}
-              sx={{
-                borderWidth: "3px 3px 0 0",
-                borderRadius: " 0 5px 0 0 ",
-              }}
-            ></Box>
-          </Box>
-        </Box>
+              display={"flex"}
+              color={"white"}
+              key={index}
+              gap={4}
+              flexDirection={isEven ? "row" : "row-reverse"}
+              alignContent={"space-between"}
+            >
+              <Box width={"120px"} display={"flex"} flexDirection={"column"}>
+                <Typography variant="h6" fontSize={8} textAlign={"center"}>
+                  {title}
+                </Typography>
+                <Typography variant="p" fontSize={6} textAlign={"center"}>
+                  {description}
+                </Typography>
+                <Typography variant="p" fontSize={6} textAlign={"center"}>
+                  {date}
+                </Typography>
+              </Box>
+
+              <Box
+                width={"100px"}
+                display={"flex"}
+                justifyContent={isEven ? "flex-start" : "flex-end"}
+              >
+                <Box
+                  width={"40px"}
+                  height={"50px"}
+                  sx={{
+                    mt: "15px",
+                    borderWidth: !isLast
+                      ? isEven
+                        ? "3px 3px 0 0"
+                        : "3px 0 0 3px"
+                      : 0,
+                  }}
+                ></Box>
+              </Box>
+            </Box>
+          );
+        })}
       </Box>
     </Container>
   );
