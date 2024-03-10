ed885c6d2d9fb1ce147187e981fa44b0ea55f6f1
RamiroAcostaDev <ramiroacosta.dev@gmail.com>
2024-03-09 21:01:49 -0300

    adding new element to section

--- app/components/Education.jsx
+++ app/components/Education.jsx
@@ -1,34 +1,64 @@
-import { Container, Box , Typography} from '@mui/material'
-import { Allerta_Stencil , Bruno_Ace , Roboto} from "next/font/google";
+import { Container, Box, Typography } from "@mui/material";
 
+import { Allerta_Stencil, Bruno_Ace, Roboto } from "next/font/google";
 
-const BrunoAce= Bruno_Ace({ subsets: ["latin"], weight: ["400"],});
+const BrunoAce = Bruno_Ace({ subsets: ["latin"], weight: ["400"] });
 
+const EducationItem = [
+  {
+    title: "Full Stack JavaScrip - ReactJs",
+    description: "Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires",
+    date: "2023",
+    direction: { borderWidth: "3px 3px 0 0" },
+  },
+];
 
-export default function Projects() {
+export default function Education() {
   return (
-    <Container sx={{background: 'grey'}} id="Educación">
-                 
-    <Box 
-        height={'100vh'}
-        display={'flex'}
-        justifyContent={'center'}
-        alignItems={'center'}
-        flexDirection={'column'}>
+    <Container id="Educación">
+      <Box
+        height={"100vh"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Box
+          display={"flex"}
+          alignContent={"center"}
+          justifyContent={"center"}
+          gap={4}
+          color={"white"}
+        >
+          <Box
+            width={"100px"}
+            display={"flex"}
+            alignContent={"center"}
+            justifyContent={"center"}
+            flexDirection={"column"}
+          >
+            <Typography variant="h6" fontSize={7} textAlign={"center"}>
+              Full Stack JavaScrip - ReactJs
+            </Typography>
+            <Typography variant="p" fontSize={5} textAlign={"center"}>
+              Codo a Codo 4.0 - Gobierno de la Ciudad de Buenos Aires
+            </Typography>
+            <Typography variant="p" fontSize={5} textAlign={"center"}>
+              2023
+            </Typography>
+          </Box>
 
-        
-            <Typography 
-               variant="h3" 
-               fontSize={20} 
-               fontWeight="Bold"
-               color={'black'}
-               className={`${BrunoAce.className} antialiased`}>Education</Typography>
-                                   
-             
-               
-    </Box>
-
-</Container>
-
-  )
+          <Box width={"100px"}>
+            <Box
+              width={"50px"}
+              height={"100%"}
+              sx={{
+                borderWidth: "3px 3px 0 0",
+                borderRadius: " 0 5px 0 0 ",
+              }}
+            ></Box>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
